9186b1b29ae2e9beed6537ea514c3957c21a12f3
MHD. Hidayat <mhdhidayat511@gmail.com>
2023-06-14 16:21:46 +0700

    first ini & templating

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,13 @@
+@extends('dashboard.layouts.main')
+
+@section('container')
+<div class="layout-demo-wrapper">
+    <div class="layout-demo-placeholder">
+        <img src="/assets/img/layouts/layout-container-light.png" class="img-fluid" alt="Layout container" data-app-light-img="layouts/layout-container-light.png" data-app-dark-img="layouts/layout-container-dark.png" />
+    </div>
+    <div class="layout-demo-info">
+        <h4>Layout container</h4>
+        <p>Container layout sets a <code>max-width</code> at each responsive breakpoint.</p>
+    </div>
+</div>
+@endsection
